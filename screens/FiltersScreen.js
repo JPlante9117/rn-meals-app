c2ece51a594c0e334d164f13ce00a207c2919b5c
Jacques Plante <jacrplante@gmail.com>
2020-04-17 12:58:09 -0400

    added default filter component inside filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
+import { Switch } from 'react-native-paper'
+import DefaultText from '../components/DefaultText'
+import Colors from '../constants/Colors'
+
+const Filter = props => {
+    return(
+        <View style={styles.filterContainer}>
+            <DefaultText style={styles.filterLabel}>{props.label}</DefaultText>
+            <Switch value={props.switchStatus} onValueChange={newValue => props.setSwitchStatus(newValue)} trackColor={{true: Colors.primary}} thumbColor={Platform.OS === 'android' ? Colors.primary : ''} />
+        </View>
+    )
+}
 
 const FiltersScreen = props => {
+
+    let [isGlutenFree, setIsGlutenFree] = useState(false)
 
     React.useLayoutEffect(() => {
         props.navigation.setOptions({
@@ -16,7 +30,8 @@ const FiltersScreen = props => {
 
     return(
         <View style={styles.screen}>
-            <Text>Filters</Text>
+            <Text style={styles.title}>Available Filters</Text>
+            <Filter label="Gluten-Free" switchStatus={isGlutenFree} setSwitchStatus={setIsGlutenFree} />
         </View>
     )
 }
@@ -24,8 +39,22 @@ const FiltersScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 30,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%'
+    },
+    filterLabel: {
+        fontSize: 18
     }
 })
 

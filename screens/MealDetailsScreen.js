903737fa9a1ddf7c09bde6be55acbcf8574ba071
Jacques Plante <jacrplante@gmail.com>
2020-04-17 12:45:05 -0400

    added details to meal details screen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import CustomHeaderButton from '../components/HeaderButton'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { ScrollView, FlatList } from 'react-native-gesture-handler'
+import DefaultText from '../components/DefaultText'
+import { MEALS } from '../data/Dummy-Data'
+import { Entypo } from '@expo/vector-icons'
+import Colors from '../constants/Colors'
 
+const ListItem = props => {
+    return <View style={styles.listItem}>
+        <Text>
+            {props.children}
+        </Text>
+    </View>
+}
 
-const MealDetailsScreen = ({navigation}) => {
+const MealDetailsScreen = ({navigation, route}) => {
 
     const [favoriteStatus, setFavoriteStatus] = useState(false)
+
+    const mealId = route.params.id
+
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
@@ -26,17 +42,43 @@ const MealDetailsScreen = ({navigation}) => {
     }, [navigation, favoriteStatus, setFavoriteStatus])
 
     return(
-        <View style={styles.screen}>
-            <Text>Meal Details</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration} minutes</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => <ListItem key={ingredient}><Entypo size={20} name="dot-single" />{ingredient}</ListItem>)}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.instructions.map((step, idx) => <ListItem key={step}>{idx + 1}. {step}</ListItem>)}
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
+    title: {
+        fontWeight: "bold",
+        fontSize: 30,
+        textAlign: 'center',
+        color: Colors.secondary
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around',
+        backgroundColor: '#ccc'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
         justifyContent: 'center',
-        alignItems: 'center'
     }
 })
 
